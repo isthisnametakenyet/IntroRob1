6e760a6abdeb97d43db87bd0c2630ad12c4e7d4c
isthisnametakenyet <casid7@outlook.es>
2023-10-03 10:30:21 +0200

    neurona basica

---
+++ perceptron.py
@@ -0,0 +1,55 @@
+import sys
+import math
+
+class Neuron():
+    def __init__(self,weights,bias):
+        self.weights = weights
+        self.bias = bias
+        self.tmp = []
+        self.z = 0
+        self.output = 0
+        self.loss = 0
+
+    def addInputs(self,inputs):
+        a = 0
+        for i in self.weights:
+            #print('i: ' + i)
+            #print('inpt: ' + inputs[a])
+            self.tmp.append(int(i) * int(inputs[a]))
+            a = a+1
+
+    def calcZ(self):
+        for i in self.tmp:
+            self.z = self.z + i
+        #print(self.z)
+        self.z + self.bias
+
+    def calcSigmoid(self):
+        self.output = 1/(1+(math.exp(1)**-self.z))
+
+    def calcMax(self):
+        max(0.1 * self.z,self.z)
+
+    def calcLoss(self):
+        #funcion de Loss, derivar J(a,y)
+        self.loss = self.output - 0
+
+if __name__ == '__main__':
+    inputs = input("Input the imputs: ")
+    weights = input("Enter the weights: ")
+    bias = int(input("Enter the bias: "))
+
+    inputs = inputs.split(',')
+    weights = weights.split(',')
+
+    thisNeuron = Neuron(weights,bias)
+    thisNeuron.addInputs(inputs)
+    thisNeuron.calcZ()
+
+    func = input("Use Sigmoid Function? ")
+    if func == 'y':
+        thisNeuron.calcSigmoid()
+    else:
+        thisNeuron.calcMax()
+    print('Output: ' + str(thisNeuron.output))
+    print('Loss: ' + str(thisNeuron.loss))
